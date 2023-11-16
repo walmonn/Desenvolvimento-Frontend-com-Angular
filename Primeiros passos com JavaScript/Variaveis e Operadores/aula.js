a6e177999d71b14171a65caae106d53aa157a911
walmonn <wagner.a.monte@hotmail.com>
2023-11-15 23:37:44 -0300

    commit_002

--- Primeiros passos com JavaScript/Variaveis e Operadores/aula.js
+++ Primeiros passos com JavaScript/Variaveis e Operadores/aula.js
@@ -1,1 +1,16 @@
+let variavel = 10;//let pode ser alterado o valor da variável
+
+const pi = 3.14; // const não pode ser alterado o valor da variável
+
+variavel = 20; //substituido o valor da varialvel por 20
+
+//Operadores + - * /
+
+variavel = 20 + 30;
+variavel = 20 - 10;
+variavel = 20 * 3;
+variavel = 20 / 2;
+
+
 console.log('Hello, World');
+console.log(variavel, pi);
---
+++ Primeiros passos com JavaScript/Variaveis e Operadores/ex001.js
@@ -0,0 +1,10 @@
+/* Faça um Programa para calcular o valor de uma viagem
+
+    Você terá 3 variáveis, sendo elas:
+    1 - Preço do combustivel
+    2 - Gasto médio de combustivel por KM
+    3 - Distancia em KM da viagem
+    
+*/
+
+console.log('hello');
